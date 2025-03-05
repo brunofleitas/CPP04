9970a19ddd20844614e6074b627c2c6160299106
brunofleitas <brunofleitas2011@gmail.com>
2025-03-05 14:16:14 +0100

    finished

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bfleitas <bfleitas@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 00:34:49 by bfleitas          #+#    #+#             */
-/*   Updated: 2025/02/21 01:19:22 by bfleitas         ###   ########.fr       */
+/*   Updated: 2025/03/05 01:48:05 by bfleitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,32 @@
 int main() 
 {
     {
-        //Manual testing
-        const Animal* meta = new Animal();
-        const Animal* j = new Dog();
-        const Animal* i = new Cat();
-        std::cout << j->getType() << " " << std::endl;
-        std::cout << i->getType() << " " << std::endl;
-        i->makeSound(); //will output the cat sound!
-        j->makeSound();
-        meta->makeSound();
-        delete meta;
-        delete j;
-        delete i;
-    }
+        std::cout << "\nManual testing" << std::endl;
 
-    //Create instances
-    const Animal* zoo = new Animal();
-    const Animal* Spike = new Dog();
-    const Animal* Kira = new Cat();
-    
-    //Show types
-    std::cout << "Animal type: " << zoo->getType() << std::endl;
-    std::cout << "Dog type: " << Spike->getType() << std::endl;
-    std::cout << "Cat type: " << Kira->getType() << std::endl;
-    
-    //Make sounds
-    zoo->makeSound();
-    Spike->makeSound();
-    Kira->makeSound();
-    
-    //Free memory
-    delete zoo;
-    delete Spike;
-    delete Kira;
+        //Create instances
+        std::cout << "\nCreating instances\n" << std::endl;
+        const Animal* zoo = new Animal();
+        const Animal* Spike = new Dog();
+        const Animal* Kira = new Cat();
+        
+        //Show types
+        std::cout << "\nShowing types\n" << std::endl;
+        std::cout << "Animal type: " << zoo->getType() << std::endl;
+        std::cout << "Dog type: " << Spike->getType() << std::endl;
+        std::cout << "Cat type: " << Kira->getType() << std::endl;
+        
+        //Make sounds
+        std::cout << "\nMaking sounds\n" << std::endl;
+        zoo->makeSound();
+        Spike->makeSound();
+        Kira->makeSound();
+        
+        //Free memory
+        std::cout << "\nFreeing memory\n" << std::endl;
+        delete zoo;
+        delete Spike;
+        delete Kira;
+    }
     
     return 0;
 }
